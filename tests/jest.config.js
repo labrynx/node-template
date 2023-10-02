234321128a81c85f333e3ee2aa10a91177df4239
alessbarb <alessbarb@gmail.com>
2023-10-03 01:32:09 +0200

    chore(template): setup files

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -1,28 +1,28 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict'
+Object.defineProperty(exports, '__esModule', { value: true })
 const config = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    globals: {
-        'ts-jest': {
-            tsconfig: '../tsconfig.tests.json',
-        },
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  globals: {
+    'ts-jest': {
+      tsconfig: '../tsconfig.tests.json',
     },
-    roots: ['<rootDir>/tests'],
-    transform: {
-        '^.+\\.tsx?$': 'ts-jest',
-    },
-    testRegex: '(<rootDir>/tests/.*|(\\.|/)(test|spec))\\.tsx?$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    coverageDirectory: 'coverage',
-    collectCoverageFrom: [
-        '<rootDir>/tests/**/*.{test,spec}.{ts,tsx}',
-        '!<rootDir>/tests/**/*.d.ts',
-    ],
-    coverageReporters: ['json', 'lcov', 'text', 'clover'],
-    moduleNameMapper: {
-        '^@/(.*)$': '<rootDir>/tests/$1',
-    },
-};
-exports.default = config;
+  },
+  roots: ['<rootDir>/tests'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(<rootDir>/tests/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    '<rootDir>/tests/**/*.{test,spec}.{ts,tsx}',
+    '!<rootDir>/tests/**/*.d.ts',
+  ],
+  coverageReporters: ['json', 'lcov', 'text', 'clover'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/tests/$1',
+  },
+}
+exports.default = config
 //# sourceMappingURL=jest.config.js.map
