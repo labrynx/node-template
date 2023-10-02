a4cc0bfa25d06a47852b0b75aaf5c1ddab154875
alessbarb <alessbarb@gmail.com>
2023-10-02 23:30:33 +0200

    chore(template): setup jest config and gitignore

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,18 @@
-module.exports = {
+const config = {
   preset: 'ts-jest',
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.tests.json',
-    },
-  },
   testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['tests/**/*.{ts,tsx}', '!tests/**/*.d.ts'],
+  coverageReporters: ['json', 'lcov', 'text', 'clover'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/tests/$1',
+  },
 }
+
+export default config
